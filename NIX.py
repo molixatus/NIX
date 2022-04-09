0c1e22df2a45104442efbb7832a1a46e78b70a62
molixatus <40862618+molixatus@users.noreply.github.com>
2022-04-10 00:41:32 +0100

    Add files via upload

--- NIX.py
+++ NIX.py
@@ -1,54 +1,101 @@
-import curses
+import ctypes,os,sys,curses
+from os import system
 from curses import wrapper
 buttonPos = 0
+menulevel = 0
+menulevelmax = 1
+system('title NIX')
 
 def start_screen(stdscr):
-    stdscr.clear()
-    stdscr.addstr("Welcome to NIX game!", curses.color_pair(1))
-    stdscr.addstr("\nPress any key to begin!", curses.color_pair(2))
+    stdscr.addstr(20,10, "Welcome to NIX game!   ", curses.color_pair(2))
+    stdscr.addstr(21,10, "Press any key to begin!", curses.color_pair(2))
     stdscr.getkey()
-    stdscr.refresh()
-
-def keypos(stdscr):
     stdscr.clear()
+
+def menuPos(stdscr, b0, b1, b2, b3, b4, b5, b6, b7, b8, b9):
+
+    stdscr.addstr(20, 10, b0.replace('#', ' '), curses.color_pair(1))
+    stdscr.addstr(21, 10, b1.replace('#', ' '), curses.color_pair(1))
+    stdscr.addstr(22, 10, b2.replace('#', ' '), curses.color_pair(1))
+
     if buttonPos == 0:
-        cplist = [2,1,1]
+        stdscr.addstr(20, 10, b0.replace('#', ' '), curses.color_pair(2))
     elif buttonPos == 1:
-        cplist = [1,2,1]
+        stdscr.addstr(21, 10, b1.replace('#', ' '), curses.color_pair(2))
     elif buttonPos == 2:
-        cplist = [1,1,2]
-    stdscr.addstr('button 1', curses.color_pair(cplist[0]))
-    stdscr.addstr("\n" + 'button 2', curses.color_pair(cplist[1]))
-    stdscr.addstr("\n" + 'button 3', curses.color_pair(cplist[2]))
+        stdscr.addstr(22, 10, b2.replace('#', ' '), curses.color_pair(2))
+
     stdscr.refresh()
 
-def get_key(stdscr):
+def get_key(stdscr,k1,k2,k3):
     global buttonPos
+    global menulevel
+    k3 = k3.split(':')
     Key = stdscr.getkey()
-    if Key == 's':
+    ###                                     W Key
+    if Key.lower() == k1:
         if buttonPos == 2:
-            return
+            pass
         else:
             buttonPos = buttonPos + 1
-
-    elif Key == 'w':
+    ###                                     S Key
+    elif Key.lower() == k2:
         if buttonPos == 0:
-            return
+            pass
         else:
-          buttonPos = buttonPos - 1
+            buttonPos = buttonPos - 1
+    ###                                     Space
+    elif Key == k3[0] or Key == k3[1]:
+        if menulevel == 0:
+            if buttonPos == 0:
+                menulevel = menulevel + 1
+                buttonPos = 0
+            elif buttonPos == 2:
+                os._exit(1)
+                raise SystemExit
+                quit()
+                sys.exit()
+                exit()
+        elif menulevel == 1:
+            if buttonPos == 2:
+                menulevel = menulevel - 1
+                buttonPos = 0
 
 def main(stdscr):
+    global menulevel
+    curses.curs_set(0)
     curses.initscr()
     curses.start_color()
-    curses.init_color(curses.COLOR_RED, 23, 0, 74);
-    curses.init_pair(1,curses.COLOR_WHITE,curses.COLOR_RED)
-    curses.init_pair(2,curses.COLOR_RED,curses.COLOR_WHITE)
+    curses.init_color(curses.COLOR_GREEN, 46, 48, 55);
+    curses.init_color(curses.COLOR_WHITE, 235, 229, 206);
+    curses.init_pair(1,curses.COLOR_WHITE,curses.COLOR_GREEN)
+    curses.init_pair(2,curses.COLOR_GREEN,curses.COLOR_WHITE)
+    stdscr.bkgd(' ', curses.color_pair(1))
     start_screen(stdscr)
-    screen = curses.initscr()
-    screen.clear()
+    sys.stdout.write("\x1b]2;%s\x07" % 'teststst')
 
     while True:
-            keypos(stdscr)
-            get_key(stdscr)
+
+            while menulevel == 0:
+                menuPos(stdscr, 'Play Game#######', 'Settings########', 'Exit############','','','','','','','')
+                get_key(stdscr,'s','w',' :\n')
+                
+            while menulevel == 1:
+
+                menuPos(stdscr, 'Play Local######', 'Play Online#####', 'Back############','','','','','','','')
+                get_key(stdscr,'s','w',' :\n')
 
 wrapper(main)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ runmain.py
@@ -0,0 +1,8 @@
+buttonPos = 0
+menulevel = 0
+menulevelmax = 1
+system('title NIX')
+from NIX.__main__ import main
+
+if __name__ == '__main__':
+    main()
